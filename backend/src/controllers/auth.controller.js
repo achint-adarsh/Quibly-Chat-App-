8bccd81e44e10bc68edd9787be2fb96c03cceda7
achint-adarsh <adarshachint@gmail.com>
2026-03-24 23:28:23 +0530

    Integrates signup controller and database connection
    Switches the signup route to use a controller for improved logic separation and maintainability. Adds JSON body parsing and ensures the database connects when the server starts, preparing for scalable API development.

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,57 @@
+import { generateToken } from '../lib/utils.js';
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body
+
+    try {
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All fields are required!" })
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be atleast 6 characters long." })
+        }
+
+        //checkig if email exists already
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Please enter a valid email address." })
+        }
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "User with this email already exists." })
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword
+        });
+
+        if (newUser) {
+            generateToken(newUser._id, res)
+            await newUser.save()
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+            })
+
+        } else {
+            res.status(400).json({ message: "Error creating user!" })
+        }
+
+
+
+    } catch (error) {
+        console.error("Error in signup controller:", error);
+        res.status(500).json({ message: "Internal server error!" })
+    }
+};
---
+++ backend/src/lib/utils.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (userId, res) => {
+    const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, //7d in milliseconds
+        httpOnly: true, //prevent XSS attacks: cross-site scripting
+        sameSite: "strict", //prevent CSRF attacks: cross-site request forgery
+        secure: process.env.NODE_ENV === "development" ? false : true,
+    });
+};
+
+
+
+//In development : http//localhost:3000
+//In production : https://somedomain.com , secure here
