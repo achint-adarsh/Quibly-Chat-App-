dbd72be0e870cda17516e9bfb2b6d373c01a8928
achint-adarsh <adarshachint@gmail.com>
2026-03-31 02:21:10 +0530

    login and logout functionalities added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -56,7 +56,7 @@ export const signup = async (req, res) => {
                 await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
             } catch (error) {
                 console.error("Failed to send welcome email:", error);
-             }
+            }
         } else {
             res.status(400).json({ message: "Invalid user data!" });
         }
@@ -66,3 +66,32 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: "Internal server error!" });
     }
 };
+
+export const login = async(req, res) => {
+    const {email, password} = req.body
+
+    try{
+        const user = await User.findOne({ email })
+        if(!user) return res.status(400).json({message: "Invalid credentials"})
+         //never specify which one of email  or password is wrong for security reasons
+        const  isPasswordCorrect = await bcrypt.compare(password,user.password)
+        if(!isPasswordCorrect) return res.status(400).json({message: "Invalid credentials"})
+
+            generateToken(user._id,res)
+            res.status(200).json({
+                _id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                profilePic: user.profilePic,
+            });
+
+    }catch(error){
+        console.error("Error in login controller:",error);
+        res.status(500).json({message: "Internal server error!"})
+    }
+};
+
+export const logout = (_, res) =>{
+    res.cookie("jwt","",{maxAge:0});
+    res.status(200).json({message: "Logged out successfully"})
+}
